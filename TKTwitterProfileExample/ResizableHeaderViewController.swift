a5bc2d3350d967a16e1ea7bad8b04e9c2301b0d7
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2018-06-14 19:09:41 +0800

    Improve inset adjustment

--- TKTwitterProfileExample/ResizableHeaderViewController.swift
+++ TKTwitterProfileExample/ResizableHeaderViewController.swift
@@ -11,7 +11,8 @@ class ResizableHeaderViewController: UIViewController {
 
   @IBOutlet weak var tableView: ControlContainableTableView!
   @IBOutlet weak var headerView: UIView!
-  @IBOutlet weak var headerViewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var headerLabel: UILabel!
+  @IBOutlet weak var headerTopConstraint: NSLayoutConstraint!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,19 +28,27 @@ class ResizableHeaderViewController: UIViewController {
     let gesture = tableView.panGestureRecognizer
     headerView.addGestureRecognizer(gesture)
     view.addGestureRecognizer(gesture) // make the whole view controller scrollable
+
+    didTapButton(self)
   }
 
   @IBAction func didTapButton(_ sender: Any) {
-    headerViewHeightConstraint.constant = CGFloat(40 + arc4random() % 200)
+//    headerViewHeightConstraint.constant = CGFloat(40 + arc4random() % 200)
+    var text = "Resizable header"
+    for _ in 0...Int(arc4random()) % 10 {
+      text += "\nResizable header"
+    }
+    headerLabel.text = text
+    headerView.layoutIfNeeded()
+
+    var offset = tableView.contentOffset
+    tableView.contentInset = UIEdgeInsets(top: headerView.bounds.height, left: 0, bottom: 0, right: 0)
+    offset.y = -headerView.bounds.height
+    tableView.contentOffset = offset
   }
 
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-
-    var offset = tableView.contentOffset
-    tableView.contentInset = UIEdgeInsets(top: headerView.frame.height, left: 0, bottom: 0, right: 0)
-    offset.y = -headerView.frame.height
-    tableView.contentOffset = offset
   }
 
 }
@@ -47,9 +56,13 @@ class ResizableHeaderViewController: UIViewController {
 extension ResizableHeaderViewController: UIScrollViewDelegate {
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
     print("offset: \(scrollView.contentOffset) isScrolling: \(scrollView.isDragging)")
+    print("headerView.intrinsicContentSize.height: \(headerView.bounds.height)")
+    print("headerTopConstraint.constant: \(headerTopConstraint.constant)")
 
     if scrollView.isDragging || scrollView.isDecelerating {
-      headerViewHeightConstraint.constant = max(50, min(500, -scrollView.contentOffset.y))
+//      headerViewHeightConstraint.constant = max(50, min(500, -scrollView.contentOffset.y))
+//      headerTopConstraint.constant = max(50, -scrollView.contentOffset.y)
+      headerTopConstraint.constant = max(50, -scrollView.contentOffset.y) - headerView.bounds.height
     }
   }
 }
